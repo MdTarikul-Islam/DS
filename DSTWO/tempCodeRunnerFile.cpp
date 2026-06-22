804efdfdbeacb1bf9bfa12248caf19b3407e8bed
Taricol <mozumder9122023@gmail.com>
2026-06-23 01:21:44 +0600

    vectorIterator

---
+++ DSTWO/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+cout<<v[3]<<endl;
+
+    // cout<<v[v.size()-1]<<endl;
+    // cout<<v.back()<<endl;
--- DSTWO/vector_modifiers.cpp
+++ DSTWO/vector_modifiers.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    vector<int> v={1,2,3,2,5,2};
+    vector<int> v={1,2,3,4,5,6};
     // vector<int> v2={100,200,300};
 
     // v.insert(v.begin()+2,v2.begin(),v2.end());
@@ -11,8 +11,10 @@ int main()
 
     // replace(v.begin(),v.end()-1,2,100);
 
-    auto it=find(v.begin(),v.end(),5);
-    cout<<*it<<endl;
+    // auto it=find(v.begin(),v.end(),5);
+    // cout<<*it<<endl;
+
+
     // if(it==v.end()){
     //     cout<<"Not Found";
     // }
@@ -36,5 +38,16 @@ int main()
     // for(int x:v){
     //     cout<<x<<" ";
     // }
+
+    // cout<<v[3]<<endl;
+
+    // cout<<v[v.size()-1]<<endl;
+    // cout<<v.back()<<endl;
+
+    // cout<<v.front()<<endl;
+
+    for(auto it=v.begin();it<v.end();it++){
+        cout<<*it<<" ";
+    }
     return 0;
 }
