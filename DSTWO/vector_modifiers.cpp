2c2a3eb5c7ca1551cf5bb20fb38f7a4c2599c0d1
Taricol <mozumder9122023@gmail.com>
2026-06-23 00:56:02 +0600

    VecteorModifiers

--- DSTWO/vector_modifiers.cpp
+++ DSTWO/vector_modifiers.cpp
@@ -2,12 +2,24 @@
 using namespace std;
 int main()
 {
-    vector<int> v={1,2,3,4,5,6};
-    vector<int> v2={100,200,300};
+    vector<int> v={1,2,3,2,5,2};
+    // vector<int> v2={100,200,300};
 
     // v.insert(v.begin()+2,v2.begin(),v2.end());
 
-    v.erase(v.begin()+1,v.begin()+5);
+    // v.erase(v.begin()+1,v.begin()+5);
+
+    // replace(v.begin(),v.end()-1,2,100);
+
+    auto it=find(v.begin(),v.end(),5);
+    cout<<*it<<endl;
+    // if(it==v.end()){
+    //     cout<<"Not Found";
+    // }
+    // else{
+    //     cout<<"Found";
+    // }
+
 
     // v.pop_back();
     // v.pop_back();
@@ -21,8 +33,8 @@ int main()
     // for(int i=0;i<v2.size();i++){
     //     cout<<v2[i]<< " ";
     // }
-    for(int x:v){
-        cout<<x<<" ";
-    }
+    // for(int x:v){
+    //     cout<<x<<" ";
+    // }
     return 0;
 }
