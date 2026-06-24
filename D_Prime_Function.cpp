c3a7506e20b9b9808cd4808b638925eb361d892d
Taricol <mozumder9122023@gmail.com>
2026-06-25 04:49:25 +0600

    primefuction

--- D_Prime_Function.cpp
+++ D_Prime_Function.cpp
@@ -2,11 +2,30 @@
 using namespace std;
 void primeFunction(){
     int n;
-    
+    cin>>n;
+    bool flag=1;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            flag=0;
+            break;
+        }
+    }
+    if(n==1){
+        cout<<"NO"<<endl;
+    }
+    else{
+        if(flag==1){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
 }
 int main()
 {
     int test;
+    cin>>test;
     while(test--){
         primeFunction();
     }
