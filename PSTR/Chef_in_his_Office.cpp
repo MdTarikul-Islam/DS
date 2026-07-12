759ba4808ee62cb470a5a6392e02bdf8936d9f6c
Taricol <mozumder9122023@gmail.com>
2026-07-12 21:39:01 +0600

    chefInHisOffice

---
+++ PSTR/Chef_in_his_Office.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int x,y;
+        cin>>x>>y;
+        int workHour=x*4+y;
+        cout<<workHour<<endl;
+    }
+    return 0;
+}
