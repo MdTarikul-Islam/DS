a067d7ae93152e0bd4a87d9d4f2af23979952916
Taricol <mozumder9122023@gmail.com>
2026-07-13 19:50:42 +0600

    countTheNotebooks

---
+++ PSTR/Count_the_Notebooks.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int p;
+        cin>>p;
+        cout<<(p*1000)/100<<endl;
+    }
+    return 0;
+}
